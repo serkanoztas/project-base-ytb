734943273979a7ee7664a002acfd3fdabbc8870b
serkanoztas <serkanoztas05@gmail.com>
2026-02-28 15:48:23 +0300

    #7 user auth yapısı kuruldu

--- api/config/index.js
+++ api/config/index.js
@@ -1,7 +1,12 @@
 module.exports = {
     "PORT": process.env.PORT || "3000",
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
-    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://localhost:27017/project_base_ytb"
+    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://localhost:27017/project_base_ytb",
+    "JWT": {
+        "SECRET": "12345",
+        "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60 //günü saniyeye çevirdik token yaşam süresi
+
+    }
 }
 
-//elle terminalden değişken tanımlalamamk için npm i dotenv --save ile dotenv kurduk artık env dosyasında değişken tanımlanabilir
+//elle terminalden değişken tanımlalamamak için npm i dotenv --save ile dotenv kurduk artık env dosyasında değişken tanımlanabilir
--- api/db/models/Users.js
+++ api/db/models/Users.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const Enum = require("../../config/Enum");
+const is = require("is_js");
+const CustomError = require("../../lib/Error");
+const bcrypt = require("bcrypt-nodejs");
 
 const schema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
@@ -19,6 +23,18 @@ const schema = mongoose.Schema({
 
 class Users extends mongoose.Model {
 
+    //password kontrol
+    validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+
+    //email ve password kontrol class ile çağırmak için static verdik
+    static validateFieldsBeforeAuth(email, password) {
+        if (typeof password !== "string" || password.length < Enum.PASSWORD_LENGTH || is.not.email(email))
+            throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "email or password is wrong");
+
+        return null;
+    }
 }
 
 schema.loadClass(Users);
